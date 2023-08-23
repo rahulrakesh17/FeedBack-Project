0b7727cde8d95a1f33f72de5e2e95d75addbee66
rahulrakesh17 <rahulrakesh1702@gmail.com>
2023-08-23 18:48:49 +0530

    grerr

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function Card({children,reverse}) {
+  console.log("hello")
   return (
     <div className="card" style={{backgroundColor:reverse?"rgb(0,0,0,0.4)":"#ffff",
                                    color:reverse?"#ffff":"rgb(0,0,0,0.4)"}}>
